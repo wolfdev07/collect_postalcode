1613525d4815e9f94b85200a052722db2f2c3368
wolf <wolf@wolfbian.168.1.6>
2024-06-08 12:47:13 -0600

    [UPDATE] create functions v1

--- test.py
+++ test.py
@@ -45,16 +45,73 @@ def create_municipality(municipality_name, state_id):
     municipality_name = str(municipality_name)
     municipality_vector = vector_build(municipality_name)
 
-    municipality_data = {
-        'name': municipality_name,
-        'search_vector': municipality_vector,
-        'state_id': state_id
-    }
-    print("MUNICIPIOS")
-    print(municipality_data)
+    conn=get_db_connection()
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM zip_codes_mx_municipality WHERE state_id = %s AND name = %s", (state_id, municipality_name))
+    municipality = cur.fetchone()
+
+    if municipality is None:
+        cur.execute("INSERT INTO zip_codes_mx_municipality (state_id, name, search_vector) VALUES (%s, %s, to_tsvector(%s))",
+                    (state_id, 
+                    municipality_name, 
+                    municipality_vector, 
+                    ))
+        conn.commit()
+        cur.execute("SELECT * FROM zip_codes_mx_municipality WHERE state_id = %s AND name = %s", (state_id, municipality_name))
+        municipality = cur.fetchone()
+        print(f'Municipality {municipality[1]} created')
+    else:
+        print(f'Municipality {municipality[1]} already exists')
+    
+    return municipality
 
 
+def create_city(cities_list, municipality_id):
 
+    conn=get_db_connection()
+    cur=conn.cursor()
+
+    for city in cities_list:
+        print(f"Creando la ciudad: {city}")
+        city = str(city)
+        city_vector = vector_build(city)
+        cur.execute("SELECT * FROM zip_codes_mx_city WHERE municipality_id = %s AND name = %s", (municipality_id, city))
+        city_cursor = cur.fetchone()
+        if city_cursor is None:
+            cur.execute("INSERT INTO zip_codes_mx_city (municipality_id, name, search_vector) VALUES (%s, %s, to_tsvector(%s))",
+                        (municipality_id, 
+                        city, 
+                        city_vector, 
+                        ))
+            conn.commit()
+            cur.execute("SELECT * FROM zip_codes_mx_city WHERE municipality_id = %s AND name = %s", (municipality_id, city))
+            city_cursor = cur.fetchone()
+            print(f'City {city_cursor[1]} created')
+        else:
+            print(f'City {city} already exists')
+
+
+def create_postal_code(postal_code_list, municipality_id):
+
+    conn=get_db_connection()
+    cur=conn.cursor()
+
+    for postal_code in postal_code_list:
+        print(f"Creando el codigo postal: {postal_code}")
+        postal_code = str(postal_code)
+        cur.execute("SELECT * FROM zip_codes_mx_postalcode WHERE municipality_id = %s AND code = %s", (municipality_id, postal_code))
+        postal_code_cursor = cur.fetchone()
+        if postal_code_cursor is None:
+            cur.execute("INSERT INTO zip_codes_mx_postalcode (municipality_id, code) VALUES (%s, %s)",
+                        (municipality_id, 
+                        postal_code, 
+                        ))
+            conn.commit()
+            cur.execute("SELECT * FROM zip_codes_mx_postalcode WHERE municipality_id = %s AND code = %s", (municipality_id, postal_code))
+            postal_code_cursor = cur.fetchone()
+            print(f'Postal code {postal_code_cursor[1]} created')
+        else:
+            print(f'Postal code {postal_code_cursor[1]} already exists')
 
 
 
@@ -87,11 +144,29 @@ def create_zip_code_records(url, entity_number):
 
         if postal_codes:
             municipality_postalcode_dict[municipality] = postal_codes
-    
+
+    print(f"municipality_postalcode_dict: {municipality_postalcode_dict}")
+
+    # GUARDAR EL ESTADO CREADO O EXISTENTE
     state_on_bd = create_state(state_name, entity_number)
 
-    for municipality in municipality_city_dict:
-        create_municipality(municipality, state_on_bd[0])
+    for municipality in municipalities:
+        municipality_on_bd = create_municipality(municipality, state_on_bd[0])
+        
+        # VERIFICAR SI EL MUNICIPIO TIENE CIUDADES EN EL DICT
+        municipalities_cities = municipality_city_dict.get(municipality)
+        municipalities_postalcodes = municipality_postalcode_dict.get(municipality)
+
+        # CREAR CIUDADES Y CODIGOS POSTALES
+        if municipalities_cities:
+            create_city(municipalities_cities, municipality_on_bd[0])
+        else:
+            print(f'No cities found for {municipality}')
+        
+        if municipalities_postalcodes:
+            create_postal_code(municipalities_postalcodes, municipality_on_bd[0])
+        else:
+            print(f'No postal codes found for {municipality}')
 
 
 
@@ -99,4 +174,4 @@ def create_zip_code_records(url, entity_number):
     #print(municipality_postalcode_dict)
 
 
-create_zip_code_records(zelda, 1)
+create_zip_code_records(zelda, 18)
